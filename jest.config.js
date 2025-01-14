c272525907494002d091084f4a2430293ed8542c
NirangaNiluminda <niranganiluminda@gmail.com>
2025-01-14 18:51:45 +0530

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest'
+  },
+  moduleFileExtensions: ['js', 'jsx'],
+  testMatch: ['**/src/**/*.test.jsx'],
+  verbose: true
+};
