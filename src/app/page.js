4fe9ace83501e543cb5e1aee4f850e2c95275a2a
NirangaNiluminda <niranganiluminda@gmail.com>
2025-04-02 09:05:58 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
       <Hero
         image={sea}
         mainHeader="Are you ready for an adventure?"
-        secondaryHeader="Browse through the popular NIranga."
+        secondaryHeader="Browse through the popular NIranga1."
       />
       <PopularLocations />
       <Hero
